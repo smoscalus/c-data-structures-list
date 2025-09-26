7427c4a4d9001051744f7624805c09201aca5071
smoscalus <smoscalus@gmail.com>
2025-09-26 22:29:33 +0300

    add function declarations in inter.h

--- include/iter.h
+++ include/iter.h
@@ -2,6 +2,7 @@
 #define ITER_H
 
 #include "list.h"
+#include <stdbool.h>
 
 typedef struct
 {
@@ -13,5 +14,12 @@ iter begin(Mylist* l);
 
 char* next(iter* it);
 
+bool hasNext(iter* it);
+
+void reset(iter* it, Mylist* l);
+
+char* get(iter* it);
+
+bool equals(iter* it1, iter* it2);
 
 #endif
