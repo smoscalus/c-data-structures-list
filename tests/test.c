b029a984ed3d9692f0102f9332a0291a807421f5
smoscalus <smoscalus@gmail.com>
2025-09-30 18:26:27 +0300

    test list's funcs

--- tests/test.c
+++ tests/test.c
@@ -4,24 +4,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TRUE 1
+
 
 int main()
 {
     Mylist l = createList();
-    // headpush(&l,"hi");
-    // headpush(&l,"priv");
-    // headpush(&l,"wirld");
-    // headpush(&l,"world");
+    headpush(&l,"hi");
+    headpush(&l,"priv");
+    headpush(&l,"wirld");
+    headpush(&l,"world");
 
-
-    iter it = begin(&l);
-    char* val;
-    while((val = next(&it)) != NULL)
-    {
-        printf("%s \n", val);
+    for (iter i = begin(&l);hasNext(&i);next(&i)){
+        printf("%s \n",i.node->data);
     }
 
+    headpop(&l);  
 
+    printf("___________________________\n");
+
+    for (iter i = begin(&l);hasNext(&i);next(&i)){
+        printf("%s \n",i.node->data);
+    }
+
+    printf("___________________________\n");
+    Node* temp = find(&l, "priv");
+    printf("%s \n%s \n", temp->data, findValue(&l,"priv"));
+
+    printf("___________________________\n");
+
+    headpush(&l,"wirld");
+    headpush(&l,"world");
+
+    Node* temp2 = getByIndex(&l,2);
+    printf("%s \n%s \n", temp2->data, getValueByIndex(&l,2));
+
+    printf("___________________________\n");
+
+
+    printf("%d \n", isEmpty(&l));
+    
     return 1;
 }
